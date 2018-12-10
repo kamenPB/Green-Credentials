b29d118e04afba47aa53f114c38417fa73ba7228
Haroun Berraoui <hb16606@bristol.ac.uk>
2018-12-10 13:54:15 +0000

    Import Apache

--- intellij-repository/src/main.java
+++ intellij-repository/src/main.java
@@ -1,18 +1,9 @@
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Iterator;
-
 import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class main {
     // A method to print to the console the string contents of cell (0,0) from test.xlsx
