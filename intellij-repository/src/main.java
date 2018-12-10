6a185f00c66e6375d3280d62ce94c2084abb28df
Haroun Berraoui <hb16606@bristol.ac.uk>
2018-12-10 14:09:39 +0000

    Add functionality to read from cell

--- intellij-repository/src/main.java
+++ intellij-repository/src/main.java
@@ -1,17 +1,16 @@
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-
 public class main {
     // A method to print to the console the string contents of cell (0,0) from test.xlsx
-    public static void readExcelFile() throws IOException
+    public static void main(String [] args) throws IOException
     {
-        InputStream ExcelFileToRead = new FileInputStream("test.xlsx");
-        HSSFWorkbook wb = new HSSFWorkbook(ExcelFileToRead);
-        HSSFCell cell = wb.getSheetAt(0).getRow(0).getCell(0);
+        InputStream ExcelFileToRead = new FileInputStream("..\\Consumption Comparison.xlsx");
+        XSSFWorkbook wb = new XSSFWorkbook(ExcelFileToRead);
+        XSSFCell cell = wb.getSheetAt(0).getRow(5).getCell(3);
         System.out.print(cell.getStringCellValue() + "");
     }
 }
