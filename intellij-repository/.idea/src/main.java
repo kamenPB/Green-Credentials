23cc05873d5c118fa5623d26f5ed92405c627916
hberraoui <hberraoui@gmail.com>
2018-12-10 12:28:16 +0000

    Import Apache POI with Maven

--- intellij-repository/.idea/src/main.java
+++ intellij-repository/.idea/src/main.java
@@ -1,2 +1,6 @@
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
 public class main {
+    FileInputStream file = new FileInputStream(new File("C:\\test.xls"));
 }
