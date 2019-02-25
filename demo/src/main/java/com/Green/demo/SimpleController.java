ed91e870e4579c6833cb8d6893522c11ac3db08b
Jan Hellebo <janhellebo@gmail.com>
2019-02-25 12:52:57 +0000

    thymeleaf code added

--- demo/src/main/java/com/Green/demo/SimpleController.java
+++ demo/src/main/java/com/Green/demo/SimpleController.java
@@ -25,12 +25,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class SimpleController {
+public class SimpleController extends ConsumptionData{
     int eighty = 80;
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        model.addAttribute("eighty", 80);
+        model.addAttribute("eighty", getWasteTotal(1, 2));
         return "greeting";
     }
 
