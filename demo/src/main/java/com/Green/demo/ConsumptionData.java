406f552f12868cfc83828ed3c0273cc275869d11
mh17665 <44001130+mh17665@users.noreply.github.com>
2019-03-15 11:08:41 +0000

    Add files via upload
    make sure to add libraries from older stuff for it to work

--- demo/src/main/java/com/Green/demo/ConsumptionData.java
+++ demo/src/main/java/com/Green/demo/ConsumptionData.java
@@ -1,23 +1,34 @@
 package com.Green.demo;
 
-//import org.apache.poi.xssf.usermodel.XSSFCell;
-//import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-//
-//import java.io.FileInputStream;
-//import java.io.IOException;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class ConsumptionData {
     double waste;
 
-    public double wasteTotal(int month, int year) {
-        // Fake it til you make it, this only works for January 2018
-        // Just get the respective month and year from the Java
-        return 165.41;
+    private static XSSFWorkbook getWorkbookFromExcelFile() throws IOException {
+        return new XSSFWorkbook(new FileInputStream("..//data.xlsx")); // For now, hardcode the path
     }
-    public double wasteRecycled(int month, int year) {
+
+
+    public double wasteTotal(int month, int year) throws IOException {
         // Fake it til you make it, this only works for January 2018
         // Just get the respective month and year from the Java
-        return 149.13;
+        XSSFWorkbook wb = getWorkbookFromExcelFile();
+        XSSFCell cell = wb.getSheetAt(0).getRow(month).getCell(1);
+        double total = cell.getNumericCellValue();
+        return total;
+    }
+    public double wasteRecycled(int month, int year) throws IOException {
+        // Fake it til you make it, this only works for January 2018
+        // Just get the respective month and year from the Java
+        XSSFWorkbook wb = getWorkbookFromExcelFile();
+        XSSFCell cell = wb.getSheetAt(0).getRow(month).getCell(2);
+        double rec = cell.getNumericCellValue();
+        return rec;
     }
     public double waterConsumed(int month, int year) {
         // Fake it til you make it, this only works for January 2018
