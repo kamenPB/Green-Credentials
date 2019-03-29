dec1a7efa284dd60eedb96042fc07f6074e42bcd
mh17665 <44001130+mh17665@users.noreply.github.com>
2019-03-29 13:00:12 +0000

    Add files via upload

---
+++ DemoApplicationTests.java
@@ -0,0 +1,63 @@
+package com.Green.demo;
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests extends ConsumptionData{
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void fileLoaded() throws IOException {
+		final XSSFWorkbook expected = new XSSFWorkbook(new FileInputStream("..//data.xlsx"));
+		final XSSFWorkbook actual = getWorkbookFromExcelFile();
+		Assert.assertEquals(actual,expected);
+	}
+
+	@Test
+	public void month() {
+		final int expected = 2; // 1 == February
+		final int actual = getLastMonth();
+		Assert.assertEquals(actual,expected);
+	}
+
+	@Test
+	public void year() {
+		final String expected = "2017";
+		final String actual = getTwoYearsAgo();
+		Assert.assertEquals(actual,expected);
+	}
+
+	@Test
+	public void year2() {
+		final String expected = "2018";
+		final String actual = getLastYear();
+		Assert.assertEquals(actual,expected);
+	}
+
+	@Test
+	public void year3() {
+		final String expected = "2019";
+		final String actual = getCurrentYear();
+		Assert.assertEquals(actual,expected);
+	}
+
+	@Test
+	public void comment() throws IOException {
+		final String expected = "Every week, we donate leftover clothes hangers to you, our customers! Find them by the Customer Service Desk.";
+		final String actual = (String) ConsumptionData.comment("waste");
+		Assert.assertEquals(actual,expected);
+	}
+
+}
