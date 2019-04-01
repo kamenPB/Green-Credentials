60e0b6795cf163a12dce17508e5cfc15fd3f18e6
Kamen Bachvarov <kamen981@hotmail.com>
2019-04-01 11:19:05 +0100

    added tests for all years

--- demo/src/test/java/com/Green/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/Green/demo/DemoApplicationTests.java
@@ -34,9 +34,17 @@ public class DemoApplicationTests extends ConsumptionData{
 
 	@Test
 	public void year() {
-		final String expected = "2017";
-		final String actual = getTwoYearsAgo();
-		Assert.assertEquals(actual,expected);
+		final String expected2017 = "2017";
+		final String actual2017 = getTwoYearsAgo();
+		Assert.assertEquals(actual2017,expected2017);
+
+		final String expected2018 = "2018";
+		final String actual2018 = getLastYear();
+		Assert.assertEquals(actual2018,expected2018);
+
+		final String expected2019 = "2019";
+		final String actual2019 = getCurrentYear();
+		Assert.assertEquals(actual2019,expected2019);
 	}
 
 //	@Test
